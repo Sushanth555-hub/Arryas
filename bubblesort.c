a257ffc50b7c4857f84be2c133cf4de6e4ebed51
Sushanth555-hub <sushanthsushanth145@gmail.com>
2025-05-11 17:40:30 +0530

    Modified bubble sorting

--- bubblesort.c
+++ bubblesort.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 int main(){
     int n,arr[100],temp;
-    printf("Enetr the size of an array ");
-    sacnf("%d",&n);
+    printf("Enter the size of an array ");
+    scanf("%d",&n);
 
     //input the array elements
     for(int i=0;i<n;i++){
         printf("Enter the %d Element ",i+1);
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
     }
 
     //Comparing the two elements in an array
-    for(int i=0;i<n;i++){
-        for(int j=1;j<n;j++){
-            if(arr[i]>arr[j]){
-             temp=arr[i];
-             arr[i]=arr[j];
-             arr[j]=temp;
+    for(int i=0;i<n-1;i++){
+        int flage=0;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+             temp=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=temp;
+             flage=1;
             }
-            n--;
+        }
+        if(!flage){
+            break;
         }
     }
+
+
     for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
